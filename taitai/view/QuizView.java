ad118955ed51038f0d3340b5213079f1d10f307b
cornierspring <32354488+cornierspring@users.noreply.github.com>
2017-10-01 15:04:30 +1300

    Add files via upload

--- taitai/view/QuizView.java
+++ taitai/view/QuizView.java
@@ -21,10 +21,10 @@ public class QuizView {
 
 	private Scene _quiz;
 	private int _number, _questionNumber, _level, _numCorrect;
-	private boolean _clickedRecord, _firstTry, _correct;
-	private Button _record, _listen, _submit;;
+	private boolean _clickedRecord, _firstTry, _correct, _isAdded;
+	private Button _record, _listen, _submit;
 	private String _wordSaid;
-	
+
 	/*
 	 * constructor
 	 */
@@ -35,28 +35,29 @@ public class QuizView {
 		_level = level;
 		_numCorrect = numCorrect;
 		_number = number;
+		_isAdded = false;
 	}
-	
+
 	/*
 	 * gets everthing for approbriate gui
 	 */
 	public Scene getQuizView(int width, int height) {
-		
+
 		// setting up elements of gui
 		BorderPane layout;
 		Label question;
 		Button toMenu;
 		VBox questionLayout, toMenuLayout, buttonsLayout;
-		
+
 		layout = new BorderPane();
 		toMenu = new Button("Go Back to Menu");
 		_record = new Button("Record");
 		question = new Label(_number + "");
-		
+
 		questionLayout = new VBox();
 		toMenuLayout = new VBox();
 		buttonsLayout = new VBox(20);
-		
+
 		// record button event handler, records and starts timer
 		_record.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -66,11 +67,10 @@ public class QuizView {
 				_wordSaid = TaitaiModel.readRecoutFile(); // triedd to make this fit but im unable to execute code or anything so dont know if its correct
 				// just trying to read user input from mike
 				// wanted to have threads here but its too difficult to implement without being able to run any code
-				
-				
+
 				_listen = new Button("Playback");
 				_submit = new Button("Submit Answer");
-				
+
 				// submit button submits question and displays next view
 				_submit.setOnAction(new EventHandler<ActionEvent>() {
 					@Override
@@ -80,12 +80,12 @@ public class QuizView {
 						} else {
 							_correct = false;
 						}
-						
-						FeedBackView fbv = new FeedBackView(_firstTry, _questionNumber, _level, _numCorrect, correct, _number);
+
+						FeedBackView fbv = new FeedBackView(_firstTry, _questionNumber, _level, _numCorrect, _correct, _number);
 						Taitai.changeScene(fbv.getFeedBackView(width, height));
 					}
 				});
-				
+
 				// listen button event handler 
 				_listen.setOnAction(new EventHandler<ActionEvent>() {
 					@Override
@@ -93,15 +93,19 @@ public class QuizView {
 						// TODO Auto-generated method stub
 					}
 				});
+
+
+				if (!_isAdded) {
+					_listen.getStyleClass().add("button-function");
+					_submit.getStyleClass().add("button-menu");
+					buttonsLayout.getChildren().addAll(_listen, _submit);
+				}
 				
-				_listen.getStyleClass().add("button-function");
-				_submit.getStyleClass().add("button-menu");
-				
-				buttonsLayout.getChildren().addAll(_listen, _submit);
+				_isAdded = true;
 				// TODO Auto-generated method stub
 			}
 		});
-		
+
 		// to menu button
 		toMenu.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -110,32 +114,32 @@ public class QuizView {
 				Taitai.changeScene(sv.getMenuView(width, height));
 			}
 		});
-		
+
 		// formattion
 		toMenu.getStyleClass().add("button-back");
 		_record.getStyleClass().add("button-function");
 		question.getStyleClass().add("label-quiz");
-		
+
 		questionLayout.setAlignment(Pos.CENTER);
 		buttonsLayout.setAlignment(Pos.TOP_CENTER);
 		toMenuLayout.setAlignment(Pos.BOTTOM_RIGHT);
-		
+
 		buttonsLayout.setPadding(new Insets(10, 0, 20, 0));
 		toMenuLayout.setPadding(new Insets(0, 40, 40, 0));
-		
+
 		buttonsLayout.getChildren().add(_record);
 		questionLayout.getChildren().add(question);
 		toMenuLayout.getChildren().add(toMenu);
-		
+
 		layout.setTop(questionLayout);
 		layout.setCenter(buttonsLayout);
 		layout.setBottom(toMenuLayout);
-		
-		
+
+
 		_quiz = new Scene(layout, width, height);
 		_quiz.getStylesheets().add("taitai/view/TaitaiTheme.css");
 		return _quiz;
-	
+
 	}
-	
+
 }
